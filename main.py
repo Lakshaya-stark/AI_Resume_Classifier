c508380eb19c3a3c2960e9f265ca5b4332fd0dcd
Lakshaya-stark <lakstark2104@gmail.com>
2026-03-25 00:12:18 +0530

    commit 4: created API to view candidates and storing resumes in mongodb

--- main.py
+++ main.py
@@ -4,22 +4,11 @@ import os
 from utils.parser import extract_text
 from utils.nlp import extract_skills
 from utils.matcher import calculate_match_score
-from db.mongo import candidates_collection
-
-
+from db.mongo import candidates_collection  
 
 app = FastAPI()
 
 UPLOAD_DIR = "uploads"
-
-
-# Testing 
-@app.get("/test-db")
-def test_db():
-    candidates_collection.insert_one({"test": "working"})
-    return {"message": "MongoDB connected successfully"}
-
-
 
 @app.post("/upload-resume/")
 def upload_resume(
@@ -31,16 +20,30 @@ def upload_resume(
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
+
     extracted_text = extract_text(file_path)
     skills = extract_skills(extracted_text)
-
-
     score = calculate_match_score(extracted_text, job_description, skills)
+
+
+    candidate_data = {
+        "filename": file.filename,
+        "file_path": file_path,
+        "skills": skills,
+        "match_score": score,
+        "job_description": job_description,
+        "created_at": str(os.path.getctime(file_path))
+    }
+
+    candidates_collection.insert_one(candidate_data)
 
     return {
         "filename": file.filename,
         "skills": skills,
-        "match_score": f"{score}%",
-        "preview": extracted_text[:300]
+        "match_score": f"{score}%"
     }
 
+@app.get("/candidates")
+def get_candidates():
+    candidates = list(candidates_collection.find({}, {"_id": 0}))
+    return candidates

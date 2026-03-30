2221cb585fbf8a9ae5358b789da793ff7db20fe7
Lakshaya-stark <lakstark2104@gmail.com>
2026-03-30 12:33:45 +0530

    dnnsndsfh

--- db/mongo.py
+++ db/mongo.py
@@ -2,10 +2,14 @@ from pymongo import MongoClient
 import os
 
 
-MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI = "mongodb+srv://lakstark2104_db_user:Stark2104@cluster0.trnkmlw.mongodb.net/?appName=Cluster0"
 client = MongoClient(MONGO_URI)
 
 db = client["resume_db"]
 
 
 candidates_collection = db["candidates"]
+
+
+
+
